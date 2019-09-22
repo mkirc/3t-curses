4c3ff3faf71c2019b24030cc2022b78d774df0f8
mkirc <m.p.kirchner>
2019-09-22 20:40:16 +0200

    just a little change on the structure of the self.moves list

--- ai.py
+++ ai.py
@@ -3,66 +3,39 @@ from copy import deepcopy
 
 class MiniMax:
 
-	def __init__(self, board, aiPlayer):
+	def __init__(self, board, aiPlayer, huPlayer):
 
-		self.moves = {}
+		self.aiPlayer = aiPlayer
+		self.huPlayer = huPlayer
+		self.moves = []
 		self.bf = BoardFactory()
 		self.initBoard = self.bf.returnBoard(board,	aiPlayer)
 		self.initBoard.minimax()
 		self.moves = self.initBoard.moves
 
 
-	def getEmptyFields(self):
-
-		e = []
-		for i in [0, 2, 4]:
-			for j in [0, 2, 4]:
-				if self.initBoard[i][j] == 0:
-					e.append((i, j))
-		return e
-
-	def playerSwitch(self):
-
-		if self.player == 'X':
-			self.player = 'O'
-		elif self.player == 'O':
-			self.player = 'X'
-
-	def isTermState(self):
-		if self.winCondition(self.initBoard, 'X'):
-			return 1
-		elif self.winCondition(self.initBoard, 'O'):
-			return -1
-		elif len(self.emptyFields) == 0:
-			print('wtf')
-
-	def winCondition(self, board, player):
-
-		countx = 0 #check rows and columns
-		counto = 0
-
-
-		# orthogonal win condition
-		for i in [0, 2, 4]:
-			if [board[i][j] for j in [0, 2, 4]] == [player for i in range(3)]:
-				return True
-			for j in [0, 2, 4]:
-				if board[j][i] == player:
-					countx += 1
-					if countx == 3:
-						return True
-
-			counto = 0
-			countx = 0
-
-		# diagonal win condition
-		if [board[i][i] for i in [0, 2, 4]] == [player for i in range(3)]:
-			return True
-
-		elif [board[4-i][i] for i in [0, 2, 4]] == [player for i in range(3)]:
-			return True			
-
-
+	def findEndMove(self, moves, c=0):
+		
+		for m in moves:
+			self.aiScore = -10000
+			self.huScore = 10000
+			lvl = c
+			movesList = []
+			for k, v in m.items():
+				if type(v[1]) == int:
+					if lvl:
+						ws = ' '*lvl + '|' + '–'
+						print(ws, v[0], k, v[1])
+					else:
+						print(v[0], k, v[1])
+				if type(v[1]) == list:
+					if lvl:
+						ws = ' '*lvl + '|' + '+'
+						print(ws, v[0], k)
+					else:
+						print(v[0], k)
+					lvl += 1
+					self.findEndMove(v[1], lvl)
 
 class Board:
 
@@ -71,7 +44,7 @@ class Board:
 		self.board = board
 		self.score = None
 		self.player = player
-		self.moves = {}
+		self.moves = []
 		self.bf = BoardFactory()
 
 	def playerSwitch(self):
@@ -89,13 +62,21 @@ class Board:
 				newBoard.board[f[0]][f[1]] = self.player
 
 				if newBoard.isTermState():
+					move = {}
 					newBoard.score = newBoard.isTermState()
-					self.score = newBoard.score
-					self.moves[f] = self.score
+					
+					move[f] = self.player, newBoard.score
+					self.moves.append(move)
 				else:
+					move = {}
 					newBoard.playerSwitch()
 					newBoard.minimax()
-					self.moves[f] = newBoard.moves
+					if len(newBoard.moves) >= 1:
+						move[f] = self.player, newBoard.moves
+						self.moves.append(move)
+					else:
+						move[f] = self.player, 0
+						self.moves.append(move)
 				newBoard = None
 
 	def getEmptyFields(self):
@@ -115,8 +96,6 @@ class Board:
 			return -1
 		elif len(self.getEmptyFields()) == 0:
 			return 0
-
-
 
 	def winCondition(self, board, player):
 
@@ -142,7 +121,6 @@ class Board:
 		elif [board[4-i][i] for i in [0, 2, 4]] == [player for i in range(3)]:
 			return True			
 
-
 class BoardFactory:
 	def __init__(self):
 		pass
@@ -158,21 +136,21 @@ def main():
 	  o
 	'''
 
-	# b = [
-	# 	['X', 0, 'X', 0, 0], 
-	# 	[0, 0, 0, 0, 0], 
-	# 	['O', 0, 'O', 0, 0], 
-	# 	[0, 0, 0, 0, 0], 
-	# 	['X', 0, 'X', 0, 'O']
-	# 	]
-
 	b = [
 		['X', 0, 0, 0, 0], 
 		[0, 0, 0, 0, 0], 
-		['O', 0, 0, 0, 0], 
+		['O', 0, 'O', 0, 0], 
 		[0, 0, 0, 0, 0], 
-		['X', 0, 0, 0, 0]
+		['X', 0, 'X', 0, 0]
 		]
+
+	# b = [
+	# 	['X', 0, 0, 0, 0], 
+	# 	[0, 0, 0, 0, 0], 
+	# 	['O', 0, 0, 0, 0], 
+	# 	[0, 0, 0, 0, 0], 
+	# 	['X', 0, 0, 0, 0]
+	# 	]
 
 	# b = [
 	# 	['X', 0, 'X', 0, 'O'], 
@@ -182,9 +160,14 @@ def main():
 	# 	['X', 0, 'X', 0, 'O']
 	# 	]
 
-	m = MiniMax(b, 'O')
+	m = MiniMax(b, 'O', 'X')
 	# m.minimax()
-	pprint(m.moves)
+	# for i in m.moves:
+	# 	for k,v in i.items():
+	# 		print(k)
+	m.findEndMove(m.moves)
+
+	# pprint(m.moves)
 
 
 main()
